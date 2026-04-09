56ba91ac94289497d8ccdecc1d2da9bf4f3f18ee
Kevin <jekevin964@gmail.com>
2026-04-09 15:16:38 -0400

    misinformation

--- src/Loss_CCE.hpp
+++ src/Loss_CCE.hpp
@@ -11,7 +11,6 @@ public:
   Loss_CCE() = default;
 
   // Each column vector of `y_pred` represents the prediction values
-  // `y` must be a column vector containing a sparse representation of ground truth class data
   Eigen::VectorXd operator()(const Eigen::MatrixXd &y_pred, const Eigen::VectorXi &y) const;
 };
 
