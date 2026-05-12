28faf38a32c581db12b7763ceb0af38d572e0168
kevin <jekevin964@gmail.com>
2026-05-11 21:03:17 -0400

    add some comments/todo

--- include/unn/Layer_Dense.hpp
+++ include/unn/Layer_Dense.hpp
@@ -8,7 +8,7 @@ namespace unn
 struct Layer_Dense {
   const Eigen::Index n_in;
   const Eigen::Index n_out;
-  const Eigen::Index batch_size;
+  const Eigen::Index n_samples; // Batch size
 
   // Each row of `weights` represents the weights for a specific output neuron
   Eigen::MatrixXd weights;
@@ -16,9 +16,25 @@ struct Layer_Dense {
   // Each entry b_i of `biases` corresponds to the bias of neuron `i`
   Eigen::VectorXd biases;
 
-  Layer_Dense(Eigen::Index n_in, Eigen::Index n_out, Eigen::Index batch_size);
+  Layer_Dense(Eigen::Index n_in, Eigen::Index n_out, Eigen::Index n_samples);
 
-  Eigen::MatrixXd operator()(const Eigen::MatrixXd &inputs) const;
+  Eigen::MatrixXd operator()(const Eigen::MatrixXd &inputs);
+
+private:
+  Eigen::MatrixXd in;
+
+  // NOTE (hopefully im right im probably wrong)
+  // will need grad_in
+  // will need grad_weights
+  // will need grad_biases
+  // will need grad_next (gradient that comes in from the next layer; flows in during backpropagation)
+  // Assume grad_next is a matrix whose column vectors are gradient vectors corresponding to a sample
+  // i.e., shape(grad_next) = (n_out, n_samples)
+  //
+  // grad_in = weights^T * grad_next
+  // grad_weights = grad_next * inputs^T
+  // grad_biases = rowise sum of grad_next
+
 };
 } // namespace unn
 
--- src/Layer_Dense.cpp
+++ src/Layer_Dense.cpp
@@ -5,27 +5,29 @@
 namespace unn
 {
 Layer_Dense::Layer_Dense(Eigen::Index n_in, Eigen::Index n_out,
-                         Eigen::Index batch_size)
-    : n_in{n_in}, n_out{n_out}, batch_size{batch_size}
+                         Eigen::Index n_samples)
+    : n_in{n_in}, n_out{n_out}, n_samples{n_samples}
 {
   // Intialize with random weights
   weights = Eigen::MatrixXd::Random(n_out, n_in);
   biases = Eigen::VectorXd::Zero(n_out);
 }
 
-Eigen::MatrixXd Layer_Dense::operator()(const Eigen::MatrixXd &inputs) const
+Eigen::MatrixXd Layer_Dense::operator()(const Eigen::MatrixXd &inputs)
 {
   const bool valid_in_nrows = inputs.rows() == n_in;
-  const bool valid_in_ncols = inputs.cols() == batch_size;
+  const bool valid_in_ncols = inputs.cols() == n_samples;
 
   assert(((valid_in_nrows && valid_in_ncols) && "inputs has invalid size"));
 
   auto outputs = (weights * inputs).colwise() + biases;
 
   const bool valid_out_nrows = outputs.rows() == n_out;
-  const bool valid_out_ncols = outputs.cols() == batch_size;
+  const bool valid_out_ncols = outputs.cols() == n_samples;
 
   assert(((valid_out_nrows && valid_out_ncols) && "outputs has invalid size"));
+
+  in = inputs;
 
   return outputs;
 }
