66e4c6dde5ea1a1f87b08fba33de6427332854e3
kevin <jekevin964@gmail.com>
2026-06-04 22:45:48 -0400

    forward pass for cce

---
+++ tests/src/test_cce.cpp
@@ -0,0 +1,57 @@
+#include "unn/Loss_CCE.hpp"
+#include <Eigen/Core>
+#include <doctest/doctest.h>
+
+TEST_CASE("Categorical Cross Entropy loss forward pass (sparse)")
+{
+  const Eigen::Index n_classes = 3;
+  const Eigen::Index n_samples = 3;
+
+  Eigen::MatrixXd predictions(n_classes, n_samples);
+  Eigen::RowVectorXi targets(n_samples);
+  Eigen::MatrixXd expected(1, n_samples);
+
+  // clang-format off
+  targets << 0, 1, 1;
+  predictions << 0.7, 0.1, 0.02,
+                 0.1, 0.5, 0.9,
+                 0.2, 0.4, 0.08;
+  expected << 0.356674944, 0.693147181, 0.105360516;
+  // clang-format on
+
+  // Hopefully this API design isn't stupid and I won't regret it
+  // It's okay though it's not that serious
+  unn::Loss_CCE loss_cce(targets);
+  const auto loss = loss_cce(predictions);
+
+  CHECK(loss.isApprox(expected, 1e-4));
+}
+
+TEST_CASE("Categorical Cross Entropy loss forward pass (sparse)")
+{
+  const Eigen::Index n_classes = 3;
+  const Eigen::Index n_samples = 3;
+
+  Eigen::MatrixXd predictions(n_classes, n_samples);
+  Eigen::MatrixXd targets(n_classes, n_samples);
+  Eigen::MatrixXd expected(1, n_samples);
+
+  // clang-format off
+  targets << 1, 0, 0,
+             0, 1, 1,
+             0, 0, 0;
+
+  predictions << 0.7, 0.1, 0.02,
+                 0.1, 0.5, 0.9,
+                 0.2, 0.4, 0.08;
+
+  expected << 0.356674944, 0.693147181, 0.105360516;
+  // clang-format on
+
+  // Hopefully this API design isn't stupid and I won't regret it
+  // It's okay though it's not that serious
+  unn::Loss_CCE loss_cce(targets);
+  const auto loss = loss_cce(predictions);
+
+  CHECK(loss.isApprox(expected, 1e-4));
+}
